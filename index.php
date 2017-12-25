ed95c10ec447068442eba150576644f97ee86138
Shkodenko V. Taras <podlom@users.noreply.github.com>
2017-12-25 19:12:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+ini_set('display errors', 1);
+error_reporting(E_ALL);
 
 
 //require '../components/Loader.php';
@@ -12,9 +14,22 @@
 //   echo $e->getMessage(), "\n";
 //}
 
+/* @see: spl_autoload_register on http://php.net/manual/en/function.spl-autoload-register.php */
 
-ini_set('display errors', 1);
-error_reporting(E_ALL);
+spl_autoload_register(function($class) {
+    $arr = explode('/', $class);
+    $prefix = array_shift($arr);
+
+    if($prefix == 'components') {
+        $prefix_file = '../components/';
+    }
+    $file = $prefix_file .array_shift($arr) .'.php';
+
+    if(is_file($file)) {
+        require_once $file;
+    }
+});
+
 
 define('ROOT', dirname(__FILE__));
 require_once(ROOT .'/components/Router.php');
